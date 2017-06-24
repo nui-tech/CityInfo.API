bf3221f45caa13b58fb4f8a48668b0406135ec94
Rattapon Wannaphat <nui.rattapon@outlook.com>
2017-06-24 18:32:51 +1200

    commit

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+
+        //to work with parameter in url use {}
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+        }
+       
+    }
+}
---
+++ CityInfo.API/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CityInfo.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder() //hosting a web application
+                .UseKestrel()       //web server
+                .UseContentRoot(Directory.GetCurrentDirectory())    //specify content root directory that use by the web host
+                .UseIISIntegration()
+                .UseStartup<Startup>()      //startup type that use by the web host (startup.cs)
+                .UseApplicationInsights()
+                .Build();           //build web
+
+            host.Run();
+        }
+    }
+}
